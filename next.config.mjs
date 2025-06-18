4464c63b585ba9b447fb7d380747c4e16eaeb392
Jonathan <jgaletkin@gmail.com>
2025-06-18 14:59:06 +0300

    Fixed error page

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-}
-
-export default nextConfig 
+// This file is temporarily commented out to avoid config conflicts with next.config.js.
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'i.pravatar.cc',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// }
+//
+// export default nextConfig 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-} 
+// This file has been renamed to _document.tsx.bak for troubleshooting App Router build errors.
+// import { Html, Head, Main, NextScript } from 'next/document'
+//
+// export default function Document() {
+//   return (
+//     <Html>
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// } 
