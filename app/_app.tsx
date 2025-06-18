af4c0bb551c2fb9418f3d1447706a9f0ac6b2147
Jonathan <jgaletkin@gmail.com>
2025-06-18 20:02:32 +0300

    error fixes for nixpacks

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,8 +1,7 @@
-import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default appWithTranslation(MyApp) 
+export default MyApp 
